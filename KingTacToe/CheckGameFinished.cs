bfb17154119e25550ff10786bdc010e566d148b3
Jalen Fouda <jali10@hotmail.de>
2023-06-28 01:56:42 +0200

    Code refactoring new architecture

---
+++ KingTacToe/CheckGameFinished.cs
@@ -0,0 +1,75 @@
+﻿using System.Windows;
+
+namespace KingTacToe
+{
+	internal class CheckGameFinished
+	{
+		public static bool IsTheWinner(ref bool winner)
+		{
+			//Rows
+			if ((GameProps.buttons[0].Content == GameProps.buttons[1].Content) && (GameProps.buttons[1].Content == GameProps.buttons[2].Content) && (string)GameProps.buttons[0].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			if ((GameProps.buttons[3].Content == GameProps.buttons[4].Content) && (GameProps.buttons[4].Content == GameProps.buttons[5].Content) && (string)GameProps.buttons[3].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			if ((GameProps.buttons[6].Content == GameProps.buttons[7].Content) && (GameProps.buttons[7].Content == GameProps.buttons[8].Content) && (string)GameProps.buttons[6].Content != null)
+			{ return GameProps.Winner = winner = true;}
+
+			//Columns
+			if ((GameProps.buttons[0].Content == GameProps.buttons[3].Content) && (GameProps.buttons[3].Content == GameProps.buttons[6].Content) && (string)GameProps.buttons[0].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			if ((GameProps.buttons[1].Content == GameProps.buttons[4].Content) && (GameProps.buttons[4].Content == GameProps.buttons[7].Content) && (string)GameProps.buttons[1].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			if ((GameProps.buttons[2].Content == GameProps.buttons[5].Content) && (GameProps.buttons[5].Content == GameProps.buttons[8].Content) && (string)GameProps.buttons[2].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			//Diagonal
+			if ((GameProps.buttons[0].Content == GameProps.buttons[4].Content) && (GameProps.buttons[4].Content == GameProps.buttons[8].Content) && (string)GameProps.buttons[0].Content != null)
+			{ return GameProps.Winner = winner = true;}
+			if ((GameProps.buttons[6].Content == GameProps.buttons[4].Content) && (GameProps.buttons[4].Content == GameProps.buttons[2].Content) && (string)GameProps.buttons[6].Content != null)
+			{ return GameProps.Winner = winner = true;}
+
+			else { return GameProps.Winner = winner = false;}
+
+		}
+		public static bool IsTieGame(bool winner,ref int round)
+		{
+			if (round == 9 && (winner == false))
+			{
+				return true;
+			}
+			else { return false; }
+		}
+
+		public static void GameDone(MainWindow king, int index, ref int round, ref int player, ref bool winner) 
+		{
+			GameProps.Index = index;
+			GameProps.Round = round;
+			GameProps.Player = player;
+			
+			GameProps.Winner = winner;
+			if (GameProps.Winner.Equals(true))
+			{
+				if ((string)GameProps.buttons[GameProps.Index].Content == "X")
+				{
+					MessageBox.Show("Player X has won.");
+					GameProps.P1++;
+					king.Player1.Content = "P1 - X: " + GameProps.P1;
+					ClickEventsLogic.NewGame(king, ref player, ref round, ref winner);
+				}
+				else if ((string)GameProps.buttons[GameProps.Index].Content == "O")
+				{
+					MessageBox.Show("Player O has won.");
+					GameProps.P2++;
+					king.Player2.Content = "P2 - O: " + GameProps.P2;
+					ClickEventsLogic.NewGame(king, ref player, ref round, ref winner);
+				}
+			}
+			else if (IsTieGame(GameProps.Winner, ref GameProps.Round) == true)
+			{
+				MessageBox.Show("Tie Game.");
+				GameProps.TieGame++;
+				king.Draw.Content = "Draw: " + GameProps.TieGame;
+				ClickEventsLogic.NewGame(king, ref player, ref round, ref winner);
+			}
+		}
+	}
+}
--- KingTacToe/MainWindow.xaml.cs
+++ KingTacToe/MainWindow.xaml.cs
@@ -7,7 +7,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,193 +26,101 @@ namespace KingTacToe
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	/// 
-	/// //Building a TicTacToe Game in a Wpf app made of 3*3 "game" buttons, an "End button", a "Reset button and a "New Game" button.
-	//I always wanna start with the player that uses crosses (player x). Once he presses a game button a cross appears on it. We go to the second round.
-	//Then there should be a player change to the circle using player (player Y).
-	//With the same mechanics and we change player on every game button click. 
-	//Once a player has a row or a column of his sign he wins the round and he gets a point on his win counter.
-	//If there is no winner after 9 rounds then it is a draw. Add a point to that counter.
+	/// //Building a TicTacToe Game in a Wpf app made of 3*3 "game" GamePropsbuttons, an "End button", a "Reset button and a "New Game" button.
+	/// //Put it on a website as well.
 
 
 	public partial class MainWindow : Window
 	{
+		List<Button> buttons = new();
+
+		bool winner;
 		int player = 2;
 		int p1;
 		int p2 = 0;
 		int draw = 0;
 		int round = 0;
-		bool winner = false;
-		string currentPlayerMark = "X";
-		string prevPlayerMark = "O";
-
-		bool IsDraw()
-		{
-			if (round == 9 && (winner == false))
-			{ 
-				return true;
-			}
-			else { return false; }
-		}
-		bool IsWinner(bool winner)
-		{
-			//Rows
-			if ((A00.Content == A01.Content) && (A01.Content == A02.Content) && (string)A00.Content != null)
-				return winner = true;
-			if (((string)A10.Content == (string)A11.Content) && ((string)A11.Content == (string)A12.Content) && (string)A10.Content != null)
-				return winner = true;
-			if (((string)A20.Content == (string)A21.Content) && ((string)A21.Content == (string)A22.Content) && (string)A20.Content != null)
-				return winner = true;
-
-			//Columns
-			if (((string?)A00.Content == (string?)A10.Content) && ((string?)A10.Content == (string?)A20.Content) && (string?)A00.Content != null)
-				return winner = true;
-			if (((string)A01.Content == (string)A11.Content) && ((string)A11.Content == (string)A21.Content) && (string)A01.Content != null)
-				return winner = true;
-			if (((string)A02.Content == (string)A12.Content) && ((string)A12.Content == (string)A22.Content) && (string)A02.Content != null)
-				return winner = true;
-			//Diagonal
-			if (((string?)A00.Content == (string)A11.Content) && ((string)A11.Content == (string)A22.Content) && (string)A00.Content != null)
-				return winner = true;
-			if (((string?)A20.Content == (string)A11.Content) && ((string)A11.Content == (string?)A02.Content) && (string)A20.Content != null)
-				return winner = true;
-
-			else { return winner = false; }
-			
-		}
+		Button clickedButton = new();
 
 		public MainWindow()
 		{
-			
 			InitializeComponent();
+
 			var converter = new System.Windows.Media.BrushConverter();
-			var brush = (Brush)converter.ConvertFromString("#292828");
+			var brush = (Brush?)converter.ConvertFromString("#292828");
 			King.Background = brush;
-			//Fill = new SolidColorBrush(Color.FromArgb(0xff, 0xff, 0x90))
 			Player1.Foreground = Brushes.White;
 			Player2.Foreground = Brushes.White;
 			Draw.Foreground = Brushes.White;
+
 			Player1.Content = "P1 - X: " + p1;
 			Player2.Content = "P2 - O: " + p2;
 			Draw.Content = "Draw: " + draw;
+
+			if (GameProps.buttons.Count == 0) 
+			{
+				ButtonList(); 
+				// Wenn Buttons nicht hier hinzugefügt werden sondern beim zurückgehen zum Home-Menü, werden sie nicht erkannt. Wieso?
+			}
+		}
+
+		public void ButtonList()
+		{
+			ClickEventsLogic.ButtonList(King, ref buttons);
+		}
+
+		public void IsWinner()
+		{
+			CheckGameFinished.IsTheWinner(ref winner);
 		}
 
 		private void Button_Clicks(object sender, RoutedEventArgs e)
-		{						
-			Button button = (Button)sender;
-			if (button.Content == null)
-			{
-				if (player % 2 == 0)
-				{
-					button.FontStyle = FontStyles.Italic; 
-					button.Content = currentPlayerMark;
-					player++;
-					++round;
-				}
-				else
-				{
-					button.FontStyle = FontStyles.Italic;
-					button.Content = prevPlayerMark;
-					player++;
-					++round;
-				}
-				
-				if (IsWinner(winner).Equals(true))
-				{
-					if (button.Content == "X")
-					{
-						MessageBox.Show("Player X has won.");
-						p1++;
-						Player1.Content = "P1 - X: " + p1;
-						NewGameAfterWin();
-					}
-					else if (button.Content == "O")
-					{
-						MessageBox.Show("Player O has won.");
-						p2++;
-						Player2.Content = "P2 - O: " + p2;
-						NewGameAfterWin();
-					}
-				}
-				else if (IsDraw() == true)
-				{
-					MessageBox.Show("Tie Game.");
-					draw++;
-					Draw.Content = "Draw: " + draw;
-					NewGameAfterWin();
-				}
-			}
+		{
+			clickedButton = (Button)sender;
 
-			
+			if (ClickButton.button1WasClicked == true)
+			{
+				VSPlay.GameAlgorithm(clickedButton, King);
+			}
+			else if (ClickButton.buttonCWasClicked == true) 
+			{
+				CPUPlay.CPUAlgorithm(clickedButton, King);
+			}
 		}
 
-		private void end_btn_Click(object sender, RoutedEventArgs e)
+		private void End_btn_Click(object sender, RoutedEventArgs e)
 		{
 			King.Close();
 		}
 
-		private void reset_btn_Click(object sender, RoutedEventArgs e)
+		private void Reset_btn_Click(object sender, RoutedEventArgs e)
 		{
-			Button button = (Button)sender;
-			A00.Content = null;
-			A01.Content = null;
-			A02.Content = null;
-			A10.Content = null;
-			A11.Content = null;
-			A12.Content = null;
-			A20.Content = null;
-			A21.Content = null;
-			A22.Content = null;
-			player = 2;
-			p1 = 0;
-			p2 = 0;
-			draw = 0;
-			round = 0;
-			Player1.Content = "P1 - X: " + p1;
-			Player2.Content = "P2 - O: " + p2;
-			Draw.Content = "Draw: " + draw;
-		}
-		private void newGame_btn_Click(object sender, RoutedEventArgs e)
-		{
-			NewGame();
-		}
-		private void NewGame()
-		{
-			A00.Content = null;
-			A01.Content = null;
-			A02.Content = null;
-			A10.Content = null;
-			A11.Content = null;
-			A12.Content = null;
-			A20.Content = null;
-			A21.Content = null;
-			A22.Content = null;
-			round = 0;
-			player = 2;
-			Player1.Content = "P1 - X: " + p1;
-			Player2.Content = "P2 - O: " + p2;
-			Draw.Content = "Draw: " + draw;
-		}
-		private void NewGameAfterWin()
-		{
-			A00.Content = null;
-			A01.Content = null;
-			A02.Content = null;
-			A10.Content = null;
-			A11.Content = null;
-			A12.Content = null;
-			A20.Content = null;
-			A21.Content = null;
-			A22.Content = null;
-			round = 0;
-			player = 2;
-			string temp = currentPlayerMark;
-			currentPlayerMark = prevPlayerMark;
-			prevPlayerMark = temp;
+			ClickEventsLogic.Reset(King, ref player, ref round, ref winner, ref p1, ref p2, ref draw);
 			
 			Player1.Content = "P1 - X: " + p1;
 			Player2.Content = "P2 - O: " + p2;
 			Draw.Content = "Draw: " + draw;
 		}
+
+		private void NewGame_btn_Click(object sender, RoutedEventArgs e)
+		{
+			ClickEventsLogic.NewGame(King, ref player, ref round, ref winner);
+		}
+
+		public void NewGameAfterGameFinished()
+		{
+			ClickEventsLogic.NewGame(King, ref player, ref round, ref winner);
+		}
+
+		private void Return_Click(object sender, RoutedEventArgs e)
+		{
+			Home homewindow = new();
+			ClickEventsLogic.ReturnReset(ref player, ref round, ref winner, ref p1, ref p2, ref draw);
+			ClickButton.buttonCWasClicked = false;
+			ClickButton.button1WasClicked = false;
+			Close();
+			homewindow.ShowDialog();
+		}
 	}
-}
+} 
 
